9e4fbed3080e6807f935a0cdec13e91c1245fa1b
zhangjing <zhangjing@getui.com>
2018-07-16 16:41:34 +0800

    赋值运算符-加法赋值

---
+++ src/js/operator/operator.js
@@ -0,0 +1,21 @@
+//赋值运算符，简单来讲，就是将它右边操作数的值赋给它左边的操作数
+var operand;
+operand = 12;
+console.log(operand); //12
+
+//加法赋值   x += y  =>  x = x + y  operator/add_assignment.js
+
+
+//减法赋值   x -= y  =>  x = x - y  
+
+
+//乘法赋值   x *= y  =>  x = x * y
+//除法赋值   x /= y  =>  x = x / y
+//求余赋值   x %= y  =>  x = x % y
+//求幂赋值   x **= y  =>  x = x ** y
+//左移位赋值   x <<= y  =>  x = x << y
+//右移位余赋值   x >>= y  =>  x = x >> y
+//无符号右移位赋值   x >>>= y  =>  x = x >>> y
+//按位与赋值   x &= y  =>  x = x & y
+//按位异或赋值   x ^= y  =>  x = x ^ y
+//按位或赋值   x |= y  =>  x = x | y
